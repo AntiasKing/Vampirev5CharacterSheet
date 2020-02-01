24a045af961784d0a1ffe13f2cb7f53177749ed8
Antoine Briaux <antoine.briaux@epitech.eu>
2020-02-02 00:29:43 +0100

    init

---
+++ src/Scenes/CharacterSheet/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+class CharacterSheet extends React.Component {
+  render() {
+    return (
+      <Container className="CharacterSheet" fluid="true">
+
+      </Container>
+    )
+  }
+}
+
+export default CharacterSheet;
